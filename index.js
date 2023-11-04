6cb97b5a319d31cd7e7210240e75adc98af149ea
EmilianoVentura7 <223314@ids.upchiapas.edu.mx>
2023-11-04 00:34:14 -0600

    Index

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-require("dotenv").config();
-require("./src/configs/db.configs");
-
+// index.js
 const express = require('express');
-const app = express();
-const clientesRouter = require('./src/routes/clientes.router');
-const authRouter = require('./src/routes/auth.route');
+const mysql = require('mysql');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
 
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-app.use('/clientes', clientesRouter);
-app.use('/auth', authRouter);
+db.connect((error) => {
+  if (error) {
+    console.error('Error al conectar a la base de datos', error.message);
+  } else {
+    console.log('Conectado a la base de datos');
+  }
+});
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.error("El api fue escuchado en el puerto: " + PORT);
+const administradorRouter = require('./src/routes/administrador.route');
+
+app.use('/api/administrador', administradorRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
